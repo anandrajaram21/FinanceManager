3752ac963f86f45978429776747d91d11c1622f7
Anand Rajaram <anandrajaram21@gmail.com>
2022-02-24 21:05:23 +0530

    Added loading spinners

--- components/Home.js
+++ components/Home.js
@@ -1,26 +1,8 @@
-import Head from "next/head";
-import {
-  Box,
-  Heading,
-  Container,
-  Text,
-  Button,
-  Stack,
-  Icon,
-  useColorModeValue,
-  createIcon,
-} from "@chakra-ui/react";
+import { Box, Heading, Container, Text, Stack } from "@chakra-ui/react";
 
 export default function CallToActionWithAnnotation() {
   return (
     <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
       <Container maxW={"3xl"}>
         <Stack
           as={Box}
@@ -44,28 +26,6 @@ export default function CallToActionWithAnnotation() {
             the most of your money. All in one place. No more paper, no more
             pen, no more worries.
           </Text>
-          <Stack
-            direction={"column"}
-            spacing={3}
-            align={"center"}
-            alignSelf={"center"}
-            position={"relative"}
-          >
-            <Button
-              colorScheme={"blue"}
-              bg={"blue.400"}
-              rounded={"full"}
-              px={6}
-              _hover={{
-                bg: "blue.500",
-              }}
-            >
-              Get Started
-            </Button>
-            <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
-              Learn more
-            </Button>
-          </Stack>
         </Stack>
       </Container>
     </>
--- components/Transactions.js
+++ components/Transactions.js
@@ -20,6 +20,7 @@ import {
   Stack,
   Box,
   Text,
+  Spinner,
   DrawerHeader,
   DrawerOverlay,
   DrawerContent,
@@ -33,8 +34,8 @@ import { FaTrashAlt } from "react-icons/fa";
 import { Formik } from "formik";
 
 function Transactions() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [transactions, setTransactions] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const [total, setTotal] = useState(0);
@@ -71,174 +72,188 @@ function Transactions() {
   };
 
   useEffect(() => {
+    setLoading(true);
     fetchData();
   }, []);
-  return (
-    <>
-      <Center py={6}>
-        <Box
-          bg={() => {
-            if (total > 0) {
-              return "blue.400";
-            } else {
-              return "red";
-            }
-          }}
-          rounded={"md"}
-          overflow={"hidden"}
-        >
-          <Stack
-            textAlign={"center"}
-            p={6}
-            color={useColorModeValue("gray.800", "white")}
-            align={"center"}
+
+  if (!isLoading) {
+    return (
+      <>
+        <Center py={6}>
+          <Box
+            bg={() => {
+              if (total > 0) {
+                return "blue.300";
+              } else {
+                return "red";
+              }
+            }}
+            rounded={"md"}
+            overflow={"hidden"}
           >
-            <Text fontSize={"3xl"} fontWeight={800}>
-              Current Expenditure
-            </Text>
-            <Text fontSize={"4xl"} fontWeight={800}>
-              ${total}
-            </Text>
-          </Stack>
-        </Box>
-      </Center>
-      {transactions.length > 0 ? (
-        <Table marginX={"auto"} maxW={"2xl"} variant="simple">
-          <TableCaption>Your Transactions</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>Description</Th>
-              <Th>Date</Th>
-              <Th isNumeric>Amount</Th>
-              <Th>Delete</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {transactions.map((transaction) => (
-              <Tr key={transaction.id}>
-                <Td>{transaction.description}</Td>
-                <Td>{new Date(transaction.date).toUTCString()}</Td>
-                <Td isNumeric>{transaction.amount}</Td>
-                <Td>
-                  {/* <Button leftIcon={<FaTrashAlt />}></Button> */}
-                  <Center>
-                    <FaTrashAlt
-                      cursor={"pointer"}
-                      onClick={() => deleteTransaction(transaction.id)}
-                    ></FaTrashAlt>
-                  </Center>
-                </Td>
-              </Tr>
-            ))}
-          </Tbody>
-          <Tfoot>
-            <Tr>
-              <Th>Description</Th>
-              <Th>Date</Th>
-              <Th isNumeric>Amount</Th>
-              <Th>Delete</Th>
-            </Tr>
-          </Tfoot>
-        </Table>
-      ) : (
-        <Center>
-          <Heading marginY={4}>No Transactions</Heading>
-        </Center>
-      )}
-      <Drawer
-        isOpen={isOpen}
-        placement="right"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Create a Transaction</DrawerHeader>
-
-          <DrawerBody>
-            <Formik
-              initialValues={{ description: "", amount: 0 }}
-              validate={(values) => {
-                const errors = {};
-                if (!values.description) {
-                  errors.description = "Required";
-                }
-
-                if (!values.amount) {
-                  errors.amount = "Cannot be 0 or empty";
-                }
-
-                return errors;
-              }}
-              onSubmit={(values, { setSubmitting }) => {
-                fetch("/api/transaction/new", {
-                  method: "POST",
-                  body: JSON.stringify(values),
-                }).then(() => {
-                  setSubmitting(false);
-                  fetchData();
-                });
-                onClose();
-              }}
+            <Stack
+              textAlign={"center"}
+              p={6}
+              color={useColorModeValue("gray.800", "white")}
+              align={"center"}
             >
-              {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-                /* and other goodies */
-              }) => (
-                <form onSubmit={handleSubmit}>
-                  <FormControl
-                    isInvalid={errors.description && touched.description}
-                  >
-                    <FormLabel htmlFor="description">Description</FormLabel>
-                    <Input
-                      id="description"
-                      name="description"
-                      placeholder="Enter a description"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.description}
-                    />
-                    <FormErrorMessage>{errors.description}</FormErrorMessage>
-                  </FormControl>
-                  <FormControl
-                    marginY={5}
-                    isInvalid={errors.amount && touched.amount}
-                  >
-                    <FormLabel htmlFor="amount">Amount</FormLabel>
-                    <Input
-                      id="amount"
-                      name="amount"
-                      type={"number"}
-                      placeholder="Enter an amount"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.amount}
-                    />
-                    <FormErrorMessage>{errors.amount}</FormErrorMessage>
-                  </FormControl>
-                  <Button marginY={3} type="submit" disabled={isSubmitting}>
-                    Submit
-                  </Button>
-                </form>
-              )}
-            </Formik>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-      <Center marginY={5}>
-        <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
-          Create new Transaction
-        </Button>
+              <Text fontSize={"3xl"} fontWeight={800}>
+                Current Expenditure
+              </Text>
+              <Text fontSize={"4xl"} fontWeight={800}>
+                ${total}
+              </Text>
+            </Stack>
+          </Box>
+        </Center>
+        {transactions.length > 0 && !isLoading ? (
+          <Table marginX={"auto"} maxW={"2xl"} variant="simple">
+            <TableCaption>Your Transactions</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Description</Th>
+                <Th>Date</Th>
+                <Th isNumeric>Amount</Th>
+                <Th>Delete</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {transactions.map((transaction) => (
+                <Tr key={transaction.id}>
+                  <Td>{transaction.description}</Td>
+                  <Td>{new Date(transaction.date).toUTCString()}</Td>
+                  <Td isNumeric>{transaction.amount}</Td>
+                  <Td>
+                    <Center>
+                      <FaTrashAlt
+                        cursor={"pointer"}
+                        onClick={() => {
+                          deleteTransaction(transaction.id);
+                          setLoading(true);
+                        }}
+                      ></FaTrashAlt>
+                    </Center>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+            <Tfoot>
+              <Tr>
+                <Th>Description</Th>
+                <Th>Date</Th>
+                <Th isNumeric>Amount</Th>
+                <Th>Delete</Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+        ) : (
+          <Center>
+            <Heading marginY={4}>No Transactions</Heading>
+          </Center>
+        )}
+        <Drawer
+          isOpen={isOpen}
+          placement="right"
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>Create a Transaction</DrawerHeader>
+
+            <DrawerBody>
+              <Formik
+                initialValues={{ description: "", amount: 0 }}
+                validate={(values) => {
+                  const errors = {};
+                  if (!values.description) {
+                    errors.description = "Required";
+                  }
+
+                  if (!values.amount) {
+                    errors.amount = "Cannot be 0 or empty";
+                  }
+
+                  return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                  fetch("/api/transaction/new", {
+                    method: "POST",
+                    body: JSON.stringify(values),
+                  }).then(() => {
+                    setSubmitting(false);
+                    setLoading(true);
+                    fetchData();
+                  });
+                  onClose();
+                }}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+                  /* and other goodies */
+                }) => (
+                  <form onSubmit={handleSubmit}>
+                    <FormControl
+                      isInvalid={errors.description && touched.description}
+                    >
+                      <FormLabel htmlFor="description">Description</FormLabel>
+                      <Input
+                        id="description"
+                        name="description"
+                        placeholder="Enter a description"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.description}
+                      />
+                      <FormErrorMessage>{errors.description}</FormErrorMessage>
+                    </FormControl>
+                    <FormControl
+                      marginY={5}
+                      isInvalid={errors.amount && touched.amount}
+                    >
+                      <FormLabel htmlFor="amount">Amount</FormLabel>
+                      <Input
+                        id="amount"
+                        name="amount"
+                        type={"number"}
+                        placeholder="Enter an amount"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.amount}
+                      />
+                      <FormErrorMessage>{errors.amount}</FormErrorMessage>
+                    </FormControl>
+                    <Button marginY={3} type="submit" disabled={isSubmitting}>
+                      Submit
+                    </Button>
+                  </form>
+                )}
+              </Formik>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+        <Center marginY={5}>
+          <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
+            Create new Transaction
+          </Button>
+        </Center>
+      </>
+    );
+  } else {
+    return (
+      <Center>
+        <Spinner size={"xl"} marginTop={5} />
+        <Heading marginX={4}>Loading...</Heading>
       </Center>
-    </>
-  );
+    );
+  }
 }
 
 export default Transactions;
